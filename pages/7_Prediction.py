ab68275e3743219ded690ae45db67b5b26a6f749
Peter Stieg <PeterStieg@gmx.de>
2025-05-10 01:01:29 +0200

    Peter // Streamlit: Added tab 'Specific test data' for better usability in '7_Prediction.py' #12

--- pages/7_Prediction.py
+++ pages/7_Prediction.py
@@ -30,17 +30,9 @@ with st.spinner("Loading **VGG16 model**: Duration: approx. **5s**", show_time=T
 # Load DataFrame with text data
 with st.spinner("Loading **DataFrame with Text Data**", show_time=True):
     df_text_test = load_DataFrame("df_text_test.parquet")
-    df_text_test = df_text_test.sample(1)
-    row = df_text_test.iloc[0]
 
-product_id = str(row.name)
-image_id = str(row["imageid"])
-product_title = row["designation"]
-product_description = row["description"]
-image_data = [product_id, image_id, product_title]
-
-product_title_len = len(product_title)
-product_description_len = len(product_description) if product_description else 0
+# Get product IDs from DataFrame with test data
+product_ids = df_text_test.index.tolist()
 
 st.progress(7 / 8)
 st.title("Prediction")
@@ -56,9 +48,96 @@ st.markdown(
 )
 
 # Prediction content
-prediction_tab1, prediction_tab2 = st.tabs(["Random test data", "User test data"])
+prediction_tab1, prediction_tab2, prediction_tab3 = st.tabs(
+    ["Specific test data", "Random test data", "User test data"]
+)
 
 with prediction_tab1:
+    with st.expander("**Options** for test data preview"):
+        product_id_selected = st.selectbox("Select specific Product ID", product_ids)
+
+    df_text_test_specific = df_text_test.loc[[product_id_selected]]
+    row = df_text_test_specific.iloc[0]
+
+    product_id = str(row.name)
+    image_id = str(row["imageid"])
+    product_title = row["designation"]
+    product_description = row["description"]
+    image_data = [product_id, image_id, product_title]
+
+    product_title_len = len(product_title)
+    product_description_len = len(product_description) if product_description else 0
+
+    # Create a layout with columns for the processed data
+    image_proc_col, text_col, prediction_col = st.columns([1, 2, 2])
+
+    # Display product image in first column
+    with image_proc_col:
+        st.write("**Processed Image**")
+
+        display_image(image_data, option="prediction")
+
+    # Display product details in fourth column
+    with text_col:
+        st.write("**Cleaned Text Data**")
+        st.write(f"{product_title}")
+
+    with prediction_col:
+        st.write("**Predicted category**")
+        img_cpr_path = get_img_path(product_id, image_id, option="cpr")
+        st.write(f"{predict_vgg16(vgg16, img_cpr_path)}")
+
+    # ### Create a layout with columns for the original data
+    image_org_col, title_col, description_col, pid_col, iid_col = st.columns(
+        [1, 1, 1, 1, 1]
+    )
+
+    # Display product image in first column
+    with image_org_col:
+        st.write("**Original Image**")
+        display_image(image_data, option="prediction")
+
+    # Display product details in fourth column
+    with title_col:
+        st.write("**Title**")
+        st.write(f"{product_title}")
+
+    # Display product ids in second column
+    with description_col:
+        st.write("**Description**")
+        if (product_description is None) or (
+            product_description_len < product_title_len
+        ):
+            st.write(f"{product_description}")
+        else:
+            st.markdown(f"{product_description[:product_title_len-6]} *[...]*")
+
+        # Display product ids in second column
+    with pid_col:
+        st.write("**Product ID**")
+        st.write(product_id)
+
+    # Display image id in third column
+    with iid_col:
+        st.write("**Image ID**")
+        st.write(image_id)
+
+    with st.expander("View original test data"):
+        st.dataframe(pd.DataFrame(df_text_test_specific))
+
+with prediction_tab2:
+
+    df_text_test_sample = df_text_test.sample(1)
+    row = df_text_test_sample.iloc[0]
+
+    product_id = str(row.name)
+    image_id = str(row["imageid"])
+    product_title = row["designation"]
+    product_description = row["description"]
+    image_data = [product_id, image_id, product_title]
+
+    product_title_len = len(product_title)
+    product_description_len = len(product_description) if product_description else 0
 
     # Create a layout with columns for the processed data
     image_proc_col, text_col, prediction_col = st.columns([1, 2, 2])
@@ -119,13 +198,13 @@ with prediction_tab1:
 
     with footer_col1:
         with st.expander("View original test data"):
-            st.dataframe(pd.DataFrame(df_text_test))
+            st.dataframe(pd.DataFrame(df_text_test_sample))
 
     with footer_col2:
         st.button("Change product", type="primary")
 
 
-with prediction_tab2:
+with prediction_tab3:
     # Insert title
     user_product_title = st.text_input(
         "**Product title**",
