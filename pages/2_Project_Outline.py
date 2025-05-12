4726b766953c4b6137525bf3c12f4fb98854d1f1
thomas-borer <thomasborer@gmx.ch>
2025-05-12 14:41:01 +0200

    Update 2_Project_Outline.py

--- pages/2_Project_Outline.py
+++ pages/2_Project_Outline.py
@@ -44,8 +44,12 @@ with project_tab2:
         >> Distribution of product categories, text length, text content, errors, etc.
         
         :material/rule: **Data Preprocessing**
-        >> Text cleaning by e.g. removing redundant chars/ words and finding bounding boxes.
+
+        >> **1) Text cleaning**: removing html code/remnants and redundant chars/ words, solving encoding issues
         
+        >> **2) Language detection**: Using the langdetect library as well as Gemini API
+        
+        >> **2) Localization**: Using the DeepL API
       
         :material/model_training: **Modelling**
         >> **1) Feature Extraction**: Extraction of relevant features from the text and image data with e.g. TF-IDF and CNN.
@@ -92,6 +96,7 @@ with project_tab4:
             "Text classifier",
             "Text classifier",
             "Text classifier",
+            "Text classifier",
             "Image classifier",
         ],
         "Classifier": [
@@ -99,6 +104,7 @@ with project_tab4:
             "LogisticRegression",
             "XGBClassifier",
             "RandomForestClassifier",
+            "CamemBERT",
             "VGG16",
         ],
         "Accuracy": [
@@ -106,6 +112,7 @@ with project_tab4:
             0.74,
             0.65,
             0.55,
+            '-',
             0.58,
         ],
         "Precision (weighted)": [
@@ -113,6 +120,7 @@ with project_tab4:
             0.75,
             0.72,
             0.73,
+            0.76,
             0.61,
         ],
         "Recall (weighted)": [
@@ -120,6 +128,7 @@ with project_tab4:
             0.74,
             0.65,
             0.55,
+            0.75,
             0.58,
         ],
         "F1 (weighted)": [
@@ -127,6 +136,7 @@ with project_tab4:
             0.74,
             0.66,
             0.63,
+            0.75,
             0.58,
         ],
     }
@@ -153,11 +163,25 @@ with project_tab4:
 
     st.markdown(
         """
-        **Key Findings**
+        _Key Findings_
         - Underrepresented categories achieved high classification performance despite class imbalance
         - Model performance showed high sensitivity to hyperparameter selection, with significant risk of overfitting
         - Computational complexity presented scalability challenges, particularly for ensemble methods
         - Text vector representations exhibited high dimensionality but demonstrated linear separability, eliminating the need for kernel transformations
+        """
+    )
+
+    st.markdown("**CamemBERT**")
+
+    st.markdown(
+        """
+        _Key Findings_
+        * Easy to use library from HuggingFace
+        * Training is somewhat resource intensive - limited number of parameters tried
+            * tokenizer max length - 256
+            * class weight - balanced
+            * training epochs 10 - training and validation loss starting to move opposite directions
+        * potential next step - Use LLM to generate more data
         """
     )
 
